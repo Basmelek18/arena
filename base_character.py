3554419efaca314a959f3ea96482099fa7090dfc
Ispaniuk Viacheslav <basmelek18@gmail.com>
2023-11-12 11:50:29 +0300

    base start

---
+++ base_character.py
@@ -0,0 +1,30 @@
+DEFAULT_ATTACK = 5
+DEFAULT_DEFENCE = 10
+DEFAULT_HP = 100
+
+
+class BaseCharacter:
+
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self):
+        return self.set_things['sum_damage']
+
+    def defence(self):
+        return self.set_things['sum_defence']
+
+    def hells(self):
+        return self.set_things['sum_hp']
+
+    def set_things(self, *args):
+        return {
+            'sum_damage': sum(args[0]) + DEFAULT_ATTACK,
+            'sum_defence': sum(args[1]) + DEFAULT_DEFENCE,
+            'sum_hp': sum(args[2]) + DEFAULT_HP
+        }
+
+
+teo = BaseCharacter('oleg').set_things(2, 20, 10)
+print(teo.attack)
+
