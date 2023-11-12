cfdc6104e5680b4f67254148b376be1293d9a01f
Igor Dolgonosov <garik78@bk.ru>
2023-11-12 09:43:56 +0100

    add_things

---
+++ main.py
@@ -0,0 +1,46 @@
+from random import randint
+
+
+class Thing:
+    BRIEF_DESC_THING_CLASS = 'thing'
+
+    # def __init__(self, name):
+    #     self.name = name
+
+    def attack(self):
+        value_attack = randint(*self.RANGE_VALUE_ATTACK)
+        return value_attack
+
+    def defence(self):
+        value_defence = randint(*self.RANGE_VALUE_DEFENCE)
+        return value_defence
+    
+    def healing(self):
+        value_healing = randint(*self.RANGE_VALUE_HEALING)
+        return value_healing
+
+    # def __str__(self):
+    #     return f'{self.__class__.__name__} - {self.BRIEF_DESC_THING_CLASS}.'
+
+class MagicRing(Thing):
+    BRIEF_DESC_THING_CLASS = ('магическое кольцо')
+    RANGE_VALUE_ATTACK = (3, 5)
+    RANGE_VALUE_DEFENCE = (5, 10)
+
+
+class Sword(Thing):
+    BRIEF_DESC_THING_CLASS = ('меч')
+    RANGE_VALUE_ATTACK = (5, 10)
+
+
+class Shield(Thing):
+    BRIEF_DESC_THING_CLASS = ('щит')
+    RANGE_VALUE_DEFENCE = (2, 8)
+
+class Bow(Thing):
+    BRIEF_DESC_THING_CLASS = ('лук')
+    RANGE_VALUE_ATTACK = (3, 5)
+
+class Herbs(Thing):
+    BRIEF_DESC_THING_CLASS = ('магичекое зелье')
+    RANGE_VALUE_HEALING = (3, 5)
